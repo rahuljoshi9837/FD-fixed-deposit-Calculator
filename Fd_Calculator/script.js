f473a6d3bac517495144f906b6b563640d48d21f
devadmin <devadmin@domain.tvtworld.com>
2024-04-14 14:39:57 +0530

    FD Calculator

---
+++ Fd_Calculator/script.js
@@ -0,0 +1,30 @@
+function calculateMaturityAmount(){
+    const principle = parseFloat(document.querySelector("#principle").value);
+    const interestRate = parseFloat(document.querySelector("#interestRate").value);
+    const tenure = parseFloat(document.querySelector("#tenure").value);
+    // ----- Validations ----- //
+    if(isNaN(principle) || isNaN(interestRate) || isNaN(tenure)){
+        if( (isNaN(principle) && (!isNaN(interestRate) && !isNaN(tenure))) ||  isNaN(principle) ){
+            alert("The Principle value can not be null");
+            return false;
+        }
+
+        if( (isNaN(interestRate) && (!isNaN(principle) && !isNaN(tenure))) ||  isNaN(interestRate)){
+            alert("The interest Rate value can not be null");
+            return false;
+        }
+
+        if( (isNaN(tenure) && (!isNaN(principle) && !isNaN(interestRate))) ||  isNaN(tenure)){
+            alert("The interest Rate value can not be null");
+            return false;
+        }
+    }
+
+    // ----- Calculate maturity ----- //
+    const maturityAmt = principle + (principle * interestRate * tenure) / 100;
+
+    const result = document.querySelector("#result");
+    result.innerHTML = `Maturity Amount : ${maturityAmt.toFixed(2)}`;
+}
+
+document.querySelector("#calculateBtn").addEventListener("click", calculateMaturityAmount);
